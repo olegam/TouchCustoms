ceb4d3a0de65fbd37400a31fc0d7f777480fe131
Vadim Shpakovski <vadim@shpakovski.com>
2011-10-31 17:08:40 +0200

    Fixes warnings from Static Analyzer.

--- Classes/SCRSegment.h
+++ Classes/SCRSegment.h
@@ -46,6 +46,8 @@ typedef enum {
 @property (nonatomic, readonly) UIImageView *imageView;
 
 + (SCRSegment *)segmentWithStyle:(SCRSegmentStyle)style;
+
+- (id)initWithStyle:(SCRSegmentStyle)style;
 - (id)initWithStyle:(SCRSegmentStyle)style frame:(CGRect)frame;
 
 @end
